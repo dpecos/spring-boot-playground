70bbbc177923bc53183a7098939b30a5f55ba7af
Daniel Pecos Martinez <me@danielpecos.com>
2018-12-28 19:34:04 +0100

    Project initialiazed

---
+++ src/main/java/com/danielpecos/springbootplayground/SpringbootPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.danielpecos.springbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPlaygroundApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/danielpecos/springbootplayground/SpringbootPlaygroundApplicationTests.java
@@ -0,0 +1,17 @@
+package com.danielpecos.springbootplayground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
