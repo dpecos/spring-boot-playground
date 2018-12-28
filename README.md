1d4f47708b661abfc8432e4851cb21f25eec2824
Daniel Pecos Martinez <me@danielpecos.com>
2018-12-28 19:44:38 +0100

    spring-boot-devtools

---
+++ README.md
@@ -0,0 +1,11 @@
+## Spring Modules
+
+## spring-boot-devtools
+
+https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-devtools.html
+
+- Sets up some property defaults
+- Automatic restart when files in classpath change
+- Livereload
+- Global settings using a file in user's $HOME
+- Can be used with remote applications
--- src/main/java/com/danielpecos/springbootplayground/SpringbootPlaygroundApplication.java
+++ src/main/java/com/danielpecos/springbootplayground/SpringbootPlaygroundApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPlaygroundApplication.class, args);
+    }
 
 }
 
